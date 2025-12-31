e33e7bb16fe2a72410968793585065d3dc98d612
DivyaK2829 <divya29.k@gmai.com>
2025-12-31 17:10:15 +0530

    Project Java

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		int a =2829;
+		System.out.println(a);
+		String s="Divya";
+		System.out.println(s);
+	}
+
+}
