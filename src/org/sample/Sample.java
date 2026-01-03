fd7368e3e7f211652eb6039ae54378f72ba06b2a
DivyaK2829 <divya29.k@gmail.com>
2026-01-03 12:36:26 +0530

    Update Sample.java

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -6,15 +6,16 @@ public class Sample {
 		System.out.println(a);
 		String s="Divya";
 		System.out.println(s);
-<<<<<<< HEAD
-		String s2="Vijayaragavan";
-		System.out.println(s2);
-=======
+		
 		int b=2608;
 		System.out.println(b);
 		String s1="Drithi";
 		System.out.println(s1);
->>>>>>> 251ac7784b1b408444cf331959831af392a2fda7
+		
+		String s2="Vijayaragavan";
+		System.out.println(s2);
+
+
 	}
 
 }
